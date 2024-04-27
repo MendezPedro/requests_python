246dd4e79c507ccbe1c9f6df6f5a189089aac2b3
Pedro <mendez.pedro94@gmail.com>
2024-04-26 21:47:47 -0400

    all bird

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from create_html import html
 from create_css import css
 
 
-response = request_get('https://aves.ninjas.cl/api/birds')[:10]
-
+response = request_get('https://aves.ninjas.cl/api/birds')
 # extraigo los 3 datos que necesito al iterar todo el json
 lista_url = [(elemento['images']['main'], elemento['name']['english'], elemento['name']['spanish']) for elemento in response]
 texto_template = ''
